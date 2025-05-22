bf460f62759967b308ea935c6e9595bc49fa76a8
Xenoyia <xenthekobold@gmail.com>
2025-05-22 18:05:08 +0100

    Initial commit

---
+++ obj/Debug/net472/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace RaiseThoseTails
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "RaiseThoseTails";
+        public const string PLUGIN_NAME = "Raise Those Tails";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
