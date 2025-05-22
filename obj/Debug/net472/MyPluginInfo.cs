6931d0a8bdbee2ad7a7dfefda72dd85c70099c90
Xenoyia <xenthekobold@gmail.com>
2025-05-22 18:09:14 +0100

    Fixed ignores

--- obj/Debug/net472/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace RaiseThoseTails
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "RaiseThoseTails";
-        public const string PLUGIN_NAME = "Raise Those Tails";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
